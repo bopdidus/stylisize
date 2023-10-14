558a3b07000e72df514da92b976035a950fe04c0
bopdidus <bopdidus@gmail.com>
2023-10-14 16:31:35 +0200

    minify added

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const {src, dest, watch, series} = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 
+
 function buildStyles()
 {
     return src('stylisize.scss')
@@ -14,3 +15,24 @@ function watchTask()
 }
 
 exports.default = series(buildStyles, watchTask)
+
+/*
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const cleanCss = require('gulp-clean-css');
+
+// Concat and minify CSS files
+gulp.task('build-css', () => {
+    return gulp.src('css/*.css')
+    .pipe(concat('stylisize.min.css'))
+    .pipe(cleanCss())
+    .pipe(gulp.dest('build/css'));
+});
+
+gulp.task("session-start", (cb) => {
+    return gulp.series('build-css')(cb);
+});
+
+gulp.task('default', gulp.series('session-start'));
+
+*/
